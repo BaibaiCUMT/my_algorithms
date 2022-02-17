482a91b8be857ce146f07ae72f07779672f0ba44
BaibaiCUMT <zzz1987a579@163.com>
2022-02-17 21:23:22 +0800

    pull readme and collect case from PCwindows and USB

--- 4.11.2(2).py
+++
@@ -1,9 +0,0 @@
-alist = [1,3,2,4,5,6]
-
-def reverseList(list):
-    if len(list) <= 1:
-        return list
-    else:
-        return [list[-1]] + reverseList(list[:-1])
-
-print(reverseList(alist))
--- 4_2.py
+++
@@ -1,9 +0,0 @@
-a = [1,3,5,7,9]
-
-def listnum(numList):
-    if len(numList) == 1:
-        return numList[0]
-    else:
-        return numList[0] + listnum(numList[1:])
-
-print(listnum(a))
